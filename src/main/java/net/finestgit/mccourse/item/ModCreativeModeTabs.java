2715eb0ad10a9e543df08eb474d547be589cd838
FinestGit <chaserogness@gmail.com>
2025-07-24 20:11:03 -0500

    Lots of changes made, halfway through module

---
+++ src/main/java/net/finestgit/mccourse/item/ModCreativeModeTabs.java
@@ -0,0 +1,44 @@
+package net.finestgit.mccourse.item;
+
+import net.finestgit.mccourse.MCCourseMod;
+import net.finestgit.mccourse.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MCCourseMod.MODID);
+
+    public static final Supplier<CreativeModeTab> BLACK_OPAL_ITEMS_TAB =
+            CREATIVE_MODE_TABS.register("black_opal_items_tab", () -> CreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup.mccourse.black_opal_items_tab"))
+                    .icon(() -> new ItemStack(ModItems.BLACK_OPAL.get()))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.BLACK_OPAL);
+                        pOutput.accept(ModItems.RAW_BLACK_OPAL);
+                    }).build());
+    public static final Supplier<CreativeModeTab> BLACK_OPAL_BLOCKS_TAB =
+            CREATIVE_MODE_TABS.register("black_opal_blocks_tab", () -> CreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup.mccourse.black_opal_blocks_tab"))
+                    .icon(() -> new ItemStack(ModBlocks.BLACK_OPAL_BLOCK.get()))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(MCCourseMod.MODID, "black_opal_items_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModBlocks.BLACK_OPAL_BLOCK);
+                        pOutput.accept(ModBlocks.RAW_BLACK_OPAL_BLOCK);
+                        pOutput.accept(ModBlocks.BLACK_OPAL_ORE);
+                        pOutput.accept(ModBlocks.BLACK_OPAL_DEEPSLATE_ORE);
+                        pOutput.accept(ModBlocks.BLACK_OPAL_NETHER_ORE);
+                        pOutput.accept(ModBlocks.BLACK_OPAL_END_ORE);
+                    }).build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
